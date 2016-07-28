7655d97ce3f268843f1c36a0a5571a893d7bd9ce
calebogdensc <caleb.ogden@snapchat.com>
2016-07-28 12:34:09 -0700

    Updates

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-// Our Main App
+// Main entry for app
 App = function(el) {
     this.$el = $(el);
     this.$register = $('#register');
@@ -6,28 +6,29 @@ App = function(el) {
 
     $('#paws form').on('submit', this.submit.bind(this));
 
+    $('#register .showLogin').on('click', this.loginShow.bind(this));
+    $('#login .showRegister').on('click', this.registerShow.bind(this));
+
 }
 
+// Start the app
 App.prototype.init = function() {
     this.registerShow();
 }
 
+// Show Registration
 App.prototype.registerShow = function() {
-
     this.$login.hide();
     this.$register.show();
-
-    var $loginButton = $('#register .showLogin').on('click', this.loginShow.bind(this));
-    var $registerButton = $('#login .showRegister').on('click', this.registerShow.bind(this));
-
 }
 
+// Show Login
 App.prototype.loginShow = function() {
     this.$login.show();
     this.$register.hide();
 }
 
-
+// Catch all form submits
 App.prototype.submit = function(event) {
     var $form = $(event.currentTarget);
     var endpoint = $form.data('endpoint');
@@ -38,27 +39,33 @@ App.prototype.submit = function(event) {
         password: serialized[1].value
     }
 
-    // (endpoint === 'register' ? this.registerSubmit.bind(this) : this.loginSubmit.bind(this))(credentials);
+
     if (endpoint === 'register') {
         this.register(credentials)
     } else if (endpoint === 'login') {
         this.login(credentials);
     }
+
+    // (endpoint === 'register' ? this.registerSubmit.bind(this) : this.loginSubmit.bind(this))(credentials);
     return false;
 }
 
+// Firebase Register
 App.prototype.register = function(credentials) {
     var promise = firebase.auth().createUserWithEmailAndPassword(credentials.username, credentials.password);
+    // promise.then( DO SOMETHING HERE )
+    // promise.catch( DO SOMETHING HERE )
 }
 
+// Firebase login
 App.prototype.login = function(credentials) {
     var promise = firebase.auth().signInWithEmailAndPassword(credentials.username, credentials.password);
 }
 
 
+// When the DOM is ready bootstrap the app
 document.addEventListener('DOMContentLoaded', function () {
 
-    // Init the Map
     var app = new App('#paws');
     app.init();
     
